98182301f2787643ed3c94f2e0e8c3708eba1880
p.tabakov <catchexc@gmail.com>
2023-09-16 10:10:53 +0300

    project structure refactor

---
+++ backend/osm-pump/src/test/java/se/moevm/river_routes/osm/OsmPumpApplicationTests.java
@@ -0,0 +1,13 @@
+package se.moevm.river_routes.osm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsmPumpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
