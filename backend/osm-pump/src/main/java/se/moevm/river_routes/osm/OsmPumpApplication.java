e258597aba2a9c1befc70bd821d67342be580828
Natalia Strannikova <71688733+brokkko@users.noreply.github.com>
2023-09-16 13:37:26 +0300

    initialize main services (#1)
    * initialize main services
    
    * project structure refactor
    
    * add docker compose and build fixes
    
    ---------
    
    Co-authored-by: p.tabakov <catchexc@gmail.com>

---
+++ backend/osm-pump/src/main/java/se/moevm/river_routes/osm/OsmPumpApplication.java
@@ -0,0 +1,13 @@
+package se.moevm.river_routes.osm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsmPumpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsmPumpApplication.class, args);
+    }
+
+}
