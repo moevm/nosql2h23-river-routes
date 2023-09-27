1bf398d7b7bb7d740403916a56cd60c45bfde99c
Pavel <53811013+PashaThrowsException78@users.noreply.github.com>
2023-09-27 21:55:58 +0300

    Hello world (#2)
    * add pier request
    
    * add hello_world catalog
    
    ---------
    
    Co-authored-by: p.tabakov <catchexc@gmail.com>

--- backend/osm-pump/src/main/java/se/moevm/river_routes/osm/OsmPumpApplication.java
+++ backend/osm-pump/src/main/java/se/moevm/river_routes/osm/OsmPumpApplication.java
@@ -2,7 +2,11 @@ package se.moevm.river_routes.osm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableNeo4jRepositories
 @SpringBootApplication
 public class OsmPumpApplication {
 
