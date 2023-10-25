c79ce3573d3825aecf47adf4d9c2fa2d37d526da
p.tabakov <catchexc@gmail.com>
2023-10-26 00:08:10 +0300

    refactor

--- backend/osm-pump/src/main/java/se/moevm/river_routes/osm/entity/SightNode.java
+++ backend/osm-pump/src/main/java/se/moevm/river_routes/osm/entity/SightNode.java
@@ -1,8 +1,6 @@
 package se.moevm.river_routes.osm.entity;
 
-import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
@@ -12,11 +10,9 @@ import org.springframework.data.neo4j.core.schema.Relationship;
 import java.util.ArrayList;
 import java.util.List;
 
-@Builder
-@RequiredArgsConstructor
 @Getter
 @Node("Sight")
-public class SightNode implements StorableNode {
+public class SightNode implements Node2D {
 
     @Id
     @GeneratedValue
@@ -33,6 +29,12 @@ public class SightNode implements StorableNode {
 
     @Property("lon")
     private final Double lon;
+
+    public SightNode(String title, Double lat, Double lon) {
+        this.title = title;
+        this.lat = lat;
+        this.lon = lon;
+    }
 
     public void addObservationFromWater(WaterNode node) {
         availableFrom.add(node);
--- backend/osm-pump/src/main/java/se/moevm/river_routes/osm/model/Location2D.java
+++ backend/osm-pump/src/main/java/se/moevm/river_routes/osm/model/Location2D.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import se.moevm.river_routes.osm.entity.Node2D;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
-public class Location2D {
+public class Location2D implements Node2D {
 
     private Double lat;
     private Double lon;
