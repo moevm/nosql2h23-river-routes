a922097c7a3714faeb95bcdb635eed1f7cd9c0a9
NikitaEfimov0 <nikitaef2002@gmail.com>
2023-12-19 17:25:23 +0300

    added new features

--- frontend/river_routes_web_app/src/store/route/routeReducer.ts
+++ frontend/river_routes_web_app/src/store/route/routeReducer.ts
@@ -1,13 +1,28 @@
-import { DefaultState, GET_ALL_SIGHTS, routeTypes } from "@src/store/route/routeTypes";
+import {
+  CREATE_ROUTE,
+  DefaultState,
+  GET_ALL_PIERSES,
+  GET_ALL_ROUTES,
+  GET_ALL_SIGHTS,
+  routeTypes,
+} from "@src/store/route/routeTypes";
 
 const defaultState: DefaultState = {
   allSights: [],
+  allPierses: [],
+  allRoutes: [],
 };
 
 export const routeReducer = (state = defaultState, action: any) => {
   switch (action.type) {
     case GET_ALL_SIGHTS:
       return { ...state, allSights: action.payload };
+    case GET_ALL_ROUTES:
+      return { ...state, allRoutes: action.payload };
+    case GET_ALL_PIERSES:
+      return { ...state, allPierses: action.payload };
+    case CREATE_ROUTE:
+      return { ...state, allSights: [...state.allRoutes, action.payload] };
     default:
       return state;
   }
