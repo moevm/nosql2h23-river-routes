a5e29dccd425d61f0a62c5dbb8e7d6649cd8897a
brokkko <nsstrannikova@mail.ru>
2023-12-19 22:27:44 +0300

    fix controllers

--- backend/osm-pump/src/main/java/se/moevm/river_routes/osm/client/Controller.java
+++ backend/osm-pump/src/main/java/se/moevm/river_routes/osm/client/Controller.java
@@ -3,22 +3,57 @@ package se.moevm.river_routes.osm.client;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import se.moevm.river_routes.osm.dto.RouteRequest;
+import se.moevm.river_routes.osm.entity.PierNode;
+import se.moevm.river_routes.osm.entity.SightNode;
+import se.moevm.river_routes.osm.entity.WaterNode;
 import se.moevm.river_routes.osm.services.OSMPumpService;
+import se.moevm.river_routes.osm.services.ParseService;
+import se.moevm.river_routes.osm.services.RouteService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
 @Slf4j
 public class Controller {
-    private final OSMPumpService pumpService;
+    private final RouteService routeService;
+    private final ParseService parseService;
 
     @SneakyThrows
     @Scheduled(initialDelay = 1000, fixedDelay = Integer.MAX_VALUE)
     void schedule() {
-        pumpService.pumpAllData();
-//        var rr = waterRepository.getPath();
-//        System.out.println(rr);
+//        List<WaterNode> path = routeService.findPathBetweenPiersIncludeSights(
+//                parseService.getAllPierces().get(2),
+//                parseService.getAllPierces().get(3),
+//                parseService.getAllSights().stream().filter(s ->
+//                        s.getTitle().equals("Михайловский замок")
+//                                || s.getTitle().equals("Церковь Успения Пресвятой Богородицы")
+//                                || s.getTitle().equals("Ледокол Красин")).collect(Collectors.toList()));
+//        path.forEach(p -> System.out.println(p.getId()));
+    }
+
+    @GetMapping("/piers")
+    public ResponseEntity<?> getPierces() {
+        log.info("get-pierces");
+        return ResponseEntity.ok(parseService.getAllPierces());
+    }
+
+    @GetMapping("/sights")
+    public ResponseEntity<?> getSights() {
+        log.info("get-sights");
+        return ResponseEntity.ok(parseService.getAllSights());
+    }
+
+    @PostMapping("/path")
+    public ResponseEntity<?> getPath(@RequestBody RouteRequest request) {
+        log.info("get-path");
+        return ResponseEntity.ok(routeService.findPathBetweenPiersIncludeSights(
+                request.getStartPoint(), request.getEndPoint(), request.getSight()));
     }
 
 }
