64d188423b303ea0a2c28bd0d6f46c4dfe744318
p.tabakov <catchexc@gmail.com>
2023-12-22 22:12:05 +0300

    fixes

--- backend/osm-pump/src/main/java/se/moevm/river_routes/osm/OsmPumpApplication.java
+++ backend/osm-pump/src/main/java/se/moevm/river_routes/osm/OsmPumpApplication.java
@@ -3,10 +3,14 @@ package se.moevm.river_routes.osm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableScheduling
 @EnableFeignClients
+@EnableNeo4jRepositories("se.moevm.river_routes")
+@EnableTransactionManagement
 @SpringBootApplication
 public class OsmPumpApplication {
 
--- backend/osm-pump/start.sh
+++ backend/osm-pump/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-while ! nc -z db 7687; do sleep 5; done;
+while ! nc -z db 7474; do sleep 5; done;
 # запуск osm-pump.jar
 java -jar osm-pump.jar
